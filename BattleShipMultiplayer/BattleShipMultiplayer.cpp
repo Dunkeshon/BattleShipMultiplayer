c1632188717ac85ee970c0bf0559392498f036d5
Dunkeshon <andrei.ioffe@gmail.com>
2022-05-06 18:24:44 +0300

    Setting up libraries and cmake, in process

--- BattleShipMultiplayer/BattleShipMultiplayer.cpp
+++ BattleShipMultiplayer/BattleShipMultiplayer.cpp
@@ -3,10 +3,31 @@
 
 #include "BattleShipMultiplayer.h"
 
-using namespace std;
-
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+int main() {
+	std::cout<<"hello world";
+	return 1;
 }
+//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(shape);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
