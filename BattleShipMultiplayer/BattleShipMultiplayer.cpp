1e691e8d54a01ec5b059e355bc3b849c8ac6df33
Dunkeshon <andrei.ioffe@gmail.com>
2022-05-06 14:50:40 +0300

    Добавьте файлы проекта.

---
+++ BattleShipMultiplayer/BattleShipMultiplayer.cpp
@@ -0,0 +1,12 @@
+﻿// BattleShipMultiplayer.cpp: определяет точку входа для приложения.
+//
+
+#include "BattleShipMultiplayer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
